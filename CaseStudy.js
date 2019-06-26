9c9ee428df00e3789450b766947c5afe1f3dcff9
prasu074 <prasu074@gmail.com>
2019-06-26 16:57:22 +0530

    Modified

--- CaseStudy.js
+++ CaseStudy.js
@@ -8,7 +8,7 @@ var entity=[
 			{empId: 1, empname :'Abc' ,empEmail: 'abc@gmail', empRole: 'Analytic',Salary: 45000},
 			{empId: 2, empname :'Pqr' ,empEmail: 'pqr@gmail', empRole: 'System Engineer',Salary: 35000},
 			{empId: 3, empname :'Xyz' ,empEmail: 'xyz@gmail', empRole: 'Consultant',Salary: 75000},
-			{empId: 4, empname :'Lmn' ,empEmail: 'lmn@gmail', empRole: 'Manager',Salary: 95000},
+			{empId: 4, empname :'Lmno' ,empEmail: 'lmn@gmail', empRole: 'AssistantManager',Salary: 95000},
 			];
   res.send(entity);
 };
